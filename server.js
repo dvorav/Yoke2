7fa37dffaf93b81cfed275200bd5a4ffc12ac435
DPK <dougkvamme@gmail.com>
2021-01-20 13:44:15 -0600

    updated server.js

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ io.on("connection", (socket) => {
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
 db.sequelize.sync().then(function () {
-  server.listen(PORT, function () {
+  app.listen(PORT, function () {
     console.log("Server listening on: http://localhost:" + PORT);
   });
 });
