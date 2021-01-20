caba36b7944eae163e7b7a5a4ce94bce823f1fbe
DPK <dougkvamme@gmail.com>
2021-01-19 21:19:06 -0600

    moved routes into controllers

--- server.js
+++ server.js
@@ -5,19 +5,23 @@ let path = require("path");
 let app = express();
 let PORT = process.env.PORT || 3000;
 
-
-
-app.engine("handlebars", exphbs({extname: "handlebars", defaultLayout: "main", layoutsDir: __dirname + "/views/layouts" }));
+app.engine(
+  "handlebars",
+  exphbs({
+    extname: "handlebars",
+    defaultLayout: "main",
+    layoutsDir: __dirname + "/views/layouts",
+  })
+);
 app.set("view engine", "handlebars");
 
 //Serve static content for app from public directory
 app.use(express.static(path.join(__dirname, "public")));
 
 // Import routes and give the server access to them.
-let routes = require("./routes/index.js");
+let routes = require("./controllers/index.js");
 app.use(routes);
 
-
-app.listen(PORT, function() {
-    console.log("Server listening on: http://localhost:" + PORT);
+app.listen(PORT, function () {
+  console.log("Server listening on: http://localhost:" + PORT);
 });
