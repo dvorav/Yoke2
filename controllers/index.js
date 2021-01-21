1c59b7436d2042551609ea6a1a24ab170a86c01f
Benito Gonzalez <ohnobenito@gmail.com>
2021-01-21 13:31:09 -0600

    Added layout to chatroom render which will fix errors

--- controllers/index.js
+++ controllers/index.js
@@ -8,7 +8,7 @@ router.get("/", function (req, res, next) {
 
 //Renders chatroom page
 router.get("/chatroom", function (req, res, next) {
-  res.render("chatroom", { title: "Yoke Chatroom", condition: false });
+  res.render("chatroom", { title: "Yoke Chatroom", layout: "chat", condition: false });
 });
 
 module.exports = router;
