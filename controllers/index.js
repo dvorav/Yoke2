1ccd466dddb8af9cdb4c916e0bb9ad2ac1bac616
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2021-01-21 15:56:47 -0600

    Created the api routers to the database

--- controllers/index.js
+++ controllers/index.js
@@ -11,4 +11,26 @@ router.get("/chatroom", function (req, res, next) {
   res.render("chatroom", { title: "Yoke Chatroom", layout: "chat", condition: false });
 });
 
+// Get all users
+router.get("/api/all", function (req, res) {
+  // Finding all users, and then returning them to the user as JSON.
+  Chat.findAll({}).then(function (results) {
+    // results are available to us inside the .then
+    res.json(results);
+  });
+});
+
+// Add a user
+router.post("/api/new", function (req, res) {
+  
+  console.log(req.body);
+
+  Chat.create({
+    username: req.body.username,
+    topic: req.body.topic,
+  }).then(function (results) {
+    res.json(results);
+  });
+});
+
 module.exports = router;
