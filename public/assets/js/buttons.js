40194f76f2d5a7c59354e4103ab5aed681faa6c3
KELSEY <klyngdal@gmail.com>
2021-01-23 11:49:02 -0600

    style4

--- public/assets/js/buttons.js
+++ public/assets/js/buttons.js
@@ -1,18 +1,21 @@
-$(document).ready(function() {
-    function swapStyle(sheet) {
-        //added id of pagestyle to link for css style sheets
-        document.getElementById("pagestyle").setAttribute("href", sheet);
-      }
-      
-      $("#default").on("click", function(){
-        swapStyle("./assets/css/chatroom.css");
-      });
-      
-      $("#style2").on("click", function(){
-        swapStyle("./assets/css/style2.css");
-      });
-    
-      $("#style3").on("click", function() {
-          swapStyle("./assets/css/style3.css");
-      });
-    });
+$(document).ready(function () {
+  function swapStyle(sheet) {
+    //added id of pagestyle to link for css style sheets
+    document.getElementById("pagestyle").setAttribute("href", sheet);
+  }
+
+  $("#default").on("click", function () {
+    swapStyle("./assets/css/chatroom.css");
+  });
+
+  $("#style2").on("click", function () {
+    swapStyle("./assets/css/style2.css");
+  });
+
+  $("#style3").on("click", function () {
+    swapStyle("./assets/css/style3.css");
+  });
+  $("#style4").on("click", function () {
+    swapStyle("./assets/css/style4.css");
+  });
+});
